842df7853306045cb4308e2b72587fae2eab8175
Balaji Varma <prathibharani1234567890@gmail.com>
2026-01-09 20:43:42 +0530

    worked for like tokenization

--- token/token.go
+++ token/token.go
@@ -16,8 +16,17 @@ const (
 	INT   = "INT"
 
 	//Oerators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	EQ       = "=="
+	NOT_EQ   = "!="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<" //lessthan
+	GT = ">" //greater than
 
 	// Delimiters
 	COMMA     = ","
@@ -30,4 +39,26 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
